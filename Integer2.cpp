477b20ab9ba6b65c2a69d162bdfee466aae347db
Sultonov-Damir <damirsultonov.00@gmail.com>
2025-10-23 14:50:36 +0500

    Задачи 2-4

---
+++ Integer2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int M;
+	cin>>M;
+	cout<<M/1000<<" t";
+}
---
+++ Integer3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int fileSize;
+	cin>>fileSize;
+	cout<<fileSize/1024<<" Kb";
+}
